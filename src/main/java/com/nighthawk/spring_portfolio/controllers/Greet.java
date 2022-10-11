08ddee85099aea27f4e1203ac9f84ca079ead930
kar722 <karthikv722@gmail.com>
2022-10-10 21:48:00 -0700

    nflapicontroller

--- src/main/java/com/nighthawk/spring_portfolio/controllers/Greet.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Greet.java
@@ -1,8 +1,10 @@
 package com.nighthawk.spring_portfolio.controllers;
-// import java.net.URI;
-// import java.net.http.HttpClient;
-// import java.net.http.HttpRequest;
-// import java.net.http.HttpResponse;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.IOException;
+
 
 
 /* MVC code that shows defining a simple Model, calling View, and this file serving as Controller
@@ -14,9 +16,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
+
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Greet {
-
     // @GetMapping handles GET request for /greet, maps it to greeting() method
     @GetMapping("/greet")
     // @RequestParam handles variables binding to frontend, defaults, etc
@@ -33,7 +37,23 @@ public class Greet {
       // CONTROLLER handles GET request for /birds, maps it to birds() method
     @GetMapping("/news")
     public String news(Model model) {
-
+                
+        HttpRequest request = HttpRequest.newBuilder()
+		.uri(URI.create("https://nfl-schedule.p.rapidapi.com/v1/schedules"))
+		.header("X-RapidAPI-Key", "9fb1283360mshedc514375b603d6p156a26jsna7cd4ca5744a")
+		.header("X-RapidAPI-Host", "nfl-schedule.p.rapidapi.com")
+		.method("GET", HttpRequest.BodyPublishers.noBody())
+		.build();
+		HttpResponse<String> response = null;
+		try {
+			response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(response.body());
+        
         return "news";
 
     }
---
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/NFLApiController.java
@@ -0,0 +1,66 @@
+package com.nighthawk.spring_portfolio.controllers;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // annotation to create a RESTful web services
+@RequestMapping("/api/covid")  //prefix of API
+public class NFLApiController {
+    private JSONObject body; //last run result
+    private HttpStatus status; //last run status
+    String last_run = null; //last run day of month
+
+    // GET Covid 19 Stats
+    @GetMapping("/daily")   //added to end of prefix as endpoint
+    public ResponseEntity<JSONObject> getCovid() {
+
+        //calls API once a day, sets body and status properties
+        String today = new Date().toString().substring(0,10); 
+        if (last_run == null || !today.equals(last_run))
+        {
+            try {  //APIs can fail (ie Internet or Service down)
+                
+                //RapidAPI header
+                HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://nfl-schedule.p.rapidapi.com/v1/schedules"))
+                    .header("X-RapidAPI-Key", "9fb1283360mshedc514375b603d6p156a26jsna7cd4ca5744a")
+                    .header("X-RapidAPI-Host", "nfl-schedule.p.rapidapi.com")
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+
+                //RapidAPI request and response
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+                //JSONParser extracts text body and parses to JSONObject
+                this.body = (JSONObject) new JSONParser().parse(response.body());
+                this.status = HttpStatus.OK;  //200 success
+                this.last_run = today;
+            }
+            catch (Exception e) {  //capture failure info
+                HashMap<String, String> status = new HashMap<>();
+                status.put("status", "RapidApi failure: " + e);
+
+                //Setup object for error
+                this.body = (JSONObject) status;
+                this.status = HttpStatus.INTERNAL_SERVER_ERROR; //500 error
+                this.last_run = null;
+            }
+        }
+
+        //return JSONObject in RESTful style
+        return new ResponseEntity<>(body, status);
+    }
+}
+
