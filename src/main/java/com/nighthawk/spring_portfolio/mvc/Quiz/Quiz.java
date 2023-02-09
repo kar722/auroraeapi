8ae9f43541d2e019fa5c8c2460002720dea1b269
SanjayB06 <sanjaymbharadwaj@gmail.com>
2023-02-08 19:15:08 -0800

    more db functions

--- src/main/java/com/nighthawk/spring_portfolio/mvc/Quiz/Quiz.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/Quiz/Quiz.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class Quiz {
 
-    public void insert() {
+    public void insert(String user, int attempts, String player) {
         Connection c = null;
         Statement stmt = null;
         
@@ -15,8 +15,8 @@ public class Quiz {
            System.out.println("Opened database successfully");
   
            stmt = c.createStatement();
-           String sql = "INSERT INTO QUIZ (USER,ATTEMPTS,PLAYER) " +
-                          "VALUES ('him', 4, 'messi' );"; 
+           String sql = String.format("INSERT INTO QUIZ (USER,ATTEMPTS,PLAYER) " +
+                          "VALUES ('%s', %d, '%s' );",user,attempts,player); 
            stmt.executeUpdate(sql);
   
         //    sql = "INSERT INTO QUIZ (ID,NAME,AGE,ADDRESS,SALARY) " +
@@ -41,9 +41,42 @@ public class Quiz {
         System.out.println("Records created successfully");
   
     }
+    public void get_data() {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+           Class.forName("org.sqlite.JDBC");
+           c = DriverManager.getConnection("jdbc:sqlite:volumes/quiz.db");
+           c.setAutoCommit(false);
+           System.out.println("Opened database successfully");
+     
+           stmt = c.createStatement();
+           ResultSet rs = stmt.executeQuery( "SELECT * FROM QUIZ;" );
+           
+           while ( rs.next() ) {
+              int id = rs.getInt("id");
+              String  name = rs.getString("user");
+              int age  = rs.getInt("attempts");
+              String  address = rs.getString("player");
+              
+              System.out.println( "ID = " + id );
+              System.out.println( "NAME = " + name );
+              System.out.println( "ADDRESS = " + address );
+              System.out.println();
+           }
+           rs.close();
+           stmt.close();
+           c.close();
+        } catch ( Exception e ) {
+           System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+           System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+     
+    }
    public static void main( String args[] ) {
         Quiz quiz = new Quiz();
-        quiz.insert();
+        quiz.get_data();
    }
 }
 
